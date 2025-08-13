f5f0306ec35de5e1eb6fd22d6cdbdc0514002b91
현지수 <hyunzsu@naver.com>
2025-08-13 23:39:12 +0900

    feat: features/post-management 게시물 삭제 API 레이어 분리

---
+++ src/features/post-management/list/api/deletePostApi.ts
@@ -0,0 +1,10 @@
+import { api } from "../../../../shared/api"
+
+/**
+ * 게시물 삭제 API
+ * @param id - 삭제할 게시물 ID
+ * @returns Promise<void>
+ */
+export const deletePostApi = async (id: number): Promise<void> => {
+  return api.delete(`/posts/${id}`)
+}

d1dd5d81a5402d2d4686babed1c8a3eaa2426c15
현지수 <hyunzsu@naver.com>
2025-08-14 00:30:26 +0900

    feat: features/comment-management 댓글 수정 API 레이어 분리

---
+++ src/features/comment-management/update/api/updateCommentApi.ts
@@ -0,0 +1,12 @@
+import { Comment, UpdateCommentData } from "../../../../entities/comment/types"
+import { api } from "../../../../shared/api"
+
+/**
+ * 댓글 수정 API
+ * @param id - 댓글 ID
+ * @param data - 댓글 수정 데이터
+ * @returns Promise<Comment> 수정된 댓글 정보
+ */
+export const updateCommentApi = async (id: number, data: UpdateCommentData): Promise<Comment> => {
+  return api.put<Comment>(`/comments/${id}`, data)
+}

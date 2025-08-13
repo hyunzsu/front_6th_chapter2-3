2d8d11138c19e88f8299b241c58ba69af91c6905
현지수 <hyunzsu@naver.com>
2025-08-13 17:26:52 +0900

    feat: entities/tag API 레이어 분리

---
+++ src/entities/tag/api/tagApi.ts
@@ -0,0 +1,10 @@
+import { api } from "../../../shared/api"
+import { Tag } from "../types"
+
+/**
+ * 태그 목록 조회
+ * @returns Promise<Tag[]> 태그 배열
+ */
+export const getTags = async (): Promise<Tag[]> => {
+  return api.get<Tag[]>("/posts/tags")
+}

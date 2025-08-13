ea907ff70194d5808f16656f4d271ab4097a3cc8
현지수 <hyunzsu@naver.com>
2025-08-13 23:37:59 +0900

    feat: features/post-management 게시물 생성 API 레이어 분리

---
+++ src/features/post-management/create/api/createPostApi.ts
@@ -0,0 +1,11 @@
+import { api } from "../../../../shared/api"
+import { CreatePostData, PostWithAuthor } from "../../../../entities/post/types"
+
+/**
+ * 게시물 생성 API
+ * @param data - 게시물 생성 데이터
+ * @returns Promise<PostWithAuthor> 생성된 게시물 정보
+ */
+export const createPost = async (data: CreatePostData): Promise<PostWithAuthor> => {
+  return api.post<PostWithAuthor>("/posts/add", data)
+}

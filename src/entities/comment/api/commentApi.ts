cebebf83ca3634bf09564bfe1f65cb5e01a6a014
현지수 <hyunzsu@naver.com>
2025-08-13 19:55:26 +0900

    feat: entities/comment API 레이어 분리

---
+++ src/entities/comment/api/commentApi.ts
@@ -0,0 +1,11 @@
+import { api } from "../../../shared/api"
+import { CommentsResponse } from "../types"
+
+/**
+ * 특정 게시물의 댓글 목록 조회
+ * @param postId - 게시물 ID
+ * @returns Promise<CommentsResponse> 댓글 목록 응답
+ */
+export const getComments = async (postId: number): Promise<CommentsResponse> => {
+  return api.get<CommentsResponse>(`/comments/post/${postId}`)
+}

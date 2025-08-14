d8ca0b45093ed8a747d568eddfb75fce5533c75d
현지수 <hyunzsu@naver.com>
2025-08-14 19:10:23 +0900

    refactor: 캐시에 직접 새로운 댓글 추가되도록 변경

--- src/features/comment-management/create/api/createCommentApi.ts
+++ src/features/comment-management/create/api/createCommentApi.ts
@@ -1,5 +1,5 @@
 import { api } from "../../../../shared/lib"
-import { CreateCommentData, Comment } from "../../../../entities/comment/types"
+import { CreateCommentData, Comment, CommentsResponse } from "../../../../entities/comment/types"
 import { useMutation, useQueryClient } from "@tanstack/react-query"
 
 /**
@@ -15,9 +15,16 @@ export const useCreateComment = () => {
   return useMutation({
     mutationKey: ["createComment"],
     mutationFn: (data: CreateCommentData) => createComment(data),
-    onSuccess: (data) => {
-      queryClient.invalidateQueries({
-        queryKey: ["comments", data.postId],
+    onSuccess: (response, variables) => {
+      // Optimistic Update: 캐시에 직접 새 댓글 추가
+      queryClient.setQueryData(["comments", variables.postId], (oldData: CommentsResponse | undefined) => {
+        if (!oldData) return oldData
+
+        return {
+          ...oldData,
+          comments: [response, ...oldData.comments],
+          total: oldData.total + 1,
+        }
       })
     },
   })
